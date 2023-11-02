f2662ae84437db715942e87f4bf5cce59a036fa3
Zubair Afzal <76100986+emzubairafzal@users.noreply.github.com>
2023-11-02 12:36:12 +0000

    Update index.js

--- index.js
+++ index.js
@@ -5,26 +5,48 @@ for (var i=0; i<buttonList.length; i++) {
 }
 
 function getClicked() {
-  if (this.innerHTML === 'w') {
+  var buttonInnerHTML = this.innerHTML;
+  conditionsCheck(buttonInnerHTML);
+  buttonAnimation(buttonInnerHTML);
+}
+
+document.addEventListener('keydown', (event) => {
+  conditionsCheck(event.key);
+  buttonAnimation(event.key);
+});
+
+function conditionsCheck(key) {
+  
+  if (key === 'w') {
     playAudio('./sounds/crash.mp3');
-  } else if (this.innerHTML === 'a') {
+  } else if (key === 'a') {
     playAudio('./sounds/kick-bass.mp3');
-  } else if (this.innerHTML === 's') {
+  } else if (key === 's') {
     playAudio('./sounds/snare.mp3');
-  } else if (this.innerHTML === 'd') {
+  } else if (key === 'd') {
     playAudio('./sounds/tom-1.mp3');
-  } else if (this.innerHTML === 'j') {
+  } else if (key === 'j') {
     playAudio('./sounds/tom-2.mp3');
-  } else if (this.innerHTML === 'k') {
+  } else if (key === 'k') {
     playAudio('./sounds/tom-3.mp3');
-  } else if (this.innerHTML === 'l') {
+  } else if (key === 'l') {
     playAudio('./sounds/tom-4.mp3');
   } else {
-    console.log(this.innerHTML);
+    console.log(key);
   }
 }
 
 function playAudio(audioLink) {
   var audio = new Audio(audioLink);
   audio.play();
+}
+
+function buttonAnimation(currentKey) {
+  var activeButton = document.querySelector(`.${currentKey}`);
+  console.log(activeButton);
+  activeButton.classList.add("pressed");
+
+  setTimeout(function () {
+    activeButton.classList.remove("pressed");
+  }, 100);
 }
